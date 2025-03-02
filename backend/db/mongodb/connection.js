ab0ee784b8e20475c4150fdfa34a0a623e2ba11f
Neil Sagar Sahu <neilsagar55555@gmail.com>
2025-03-02 10:41:33 +0530

    log

--- backend/db/mongodb/connection.js
+++ backend/db/mongodb/connection.js
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 export const connectDB = async ()=>{
     const DB_URI = process.env.DB_URI;
     try {
+        console.log(DB_URI);
         await mongoose.connect(DB_URI);
         console.log("Connected to MongoDB");
     } catch (error) {
