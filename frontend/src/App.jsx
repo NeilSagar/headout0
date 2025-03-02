bc86aceafa7a7abf403c212f7d95a6f6fb5b1d4c
Neil Sagar Sahu <neilsagar55555@gmail.com>
2025-03-02 11:31:03 +0530

    new page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Import Routes and Route
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'; // Import Routes and Route
 import Home from './pages/Home.jsx';
 import Quiz from './pages/Quiz.jsx';
 import Score from './pages/Score.jsx';
 import ChallengePage from './pages/ChallengePage.jsx';
 import { UserProvider } from './context/UserContext.jsx';
+import Temp from './pages/Temp.jsx';
 
 
 
@@ -14,6 +15,7 @@ function App() {
       <UserProvider>
         <BrowserRouter>
           <Routes>
+            <Route path='/' element={<Temp />} />
             <Route path='/home' element={<Home />} />
             <Route path='/quiz' element={<Quiz />} />
             <Route path='/score' element={<Score />} />
---
+++ frontend/src/pages/Temp.jsx
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+function Temp(){
+    const navigate = useNavigate();
+    useEffect(()=>{
+        navigate("/home");
+    },[]);
+    return (
+        <></>
+    );
+}
+
+export default Temp;
